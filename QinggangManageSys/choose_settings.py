0499233df28c546020a2aae28fbff5d0fcfb1b83
changxincheng <changxincheng1019@gmail.com>
2017-07-24 09:06:02 +0800

    增加mac调试环境配置及master主机部署环境配置

--- QinggangManageSys/choose_settings.py
+++ QinggangManageSys/choose_settings.py
@@ -16,7 +16,7 @@ import platform
 master_ip = '10.30.0.152'
 node = platform.node()
 print(node)
-dev_machines = ('cheng-cx','cheng-cx.local')
+dev_machines = ('cheng-cx','cheng-cx.local1')
 
 if node in dev_machines:
     # folder QinggangManageSys
@@ -62,7 +62,72 @@ if node in dev_machines:
     )
     TEMPLATE_DIRS = [os.path.join(QinggangManageSys, 'templates')]
     ALLOWED_HOSTS = ['*']
-else:
+elif node == "cheng-cx.local":
+    print("单独加了一个独立的分支,以便适应现场的环境")
+    DEBUG = True
+    DATABASES = {
+        'l2own': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME':'qinggang',
+            'USER': 'qg_user',
+            'PASSWORD': '123456',
+            'HOST': master_ip,
+            'PORT': '1521',
+        },
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'qinggang',
+            'USER': 'root',
+            'PASSWORD': '123456',
+            'HOST': master_ip,
+            'PORT': '3306',
+        },
+        'mes_backup': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME': 'mesdbdg',#sid:mesdb2;service:mesdb
+            'USER': 'report_query',
+            'PASSWORD': 'xxx',
+            'HOST': '10.30.0.160',
+            'PORT': '1521',
+        },
+        'l2_backup': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME': 'qgil2dbdg',
+            'USER': 'report_query',
+            'PASSWORD': 'xxx',
+            'HOST': '10.30.0.161',
+            'PORT': '1521',
+        },
+        'sale': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME':'mesdbdg',
+            'USER': 'report_query',
+            'PASSWORD': 'xxxx',
+            'HOST': '10.30.0.160',
+            'PORT': '1521',
+        },
+    }
+    # PROJECT_DIR = '/home/maksim/qinggang/managesys'
+    PROJECT_DIR = '/Users/changxin/qinggang/managesys'
+    # MEDIA_ROOT = '/home/maksim/qinggang/media/'
+    MEDIA_ROOT = '/Users/changxin/qinggang/media/'
+    MEDIA_URL = '/media/'
+    # STATIC_ROOT = '/home/maksim/qinggang/static/'
+    STATIC_ROOT = '/Users/changxin/qinggang/static/'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_DIR, 'static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(PROJECT_DIR, 'templates'),
+    )
+
+    ALLOWED_HOSTS = [
+        '*',
+    ]
+elif node == "hadoop01":
+    print("部署在hadoop主节点")
     DEBUG = True
     DATABASES = {
         'l2own': {
@@ -110,6 +175,66 @@ else:
     MEDIA_ROOT = '/home/maksim/qinggang/media/'
     MEDIA_URL = '/media/'
     STATIC_ROOT = '/home/maksim/qinggang/static/'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_DIR, 'static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(PROJECT_DIR, 'templates'),
+    )
+
+    ALLOWED_HOSTS = [
+        '*',
+    ]
+else:
+    DEBUG = True
+    DATABASES = {
+        'l2own': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME':'qinggang',
+            'USER': 'qg_user',
+            'PASSWORD': '123456',
+            'HOST': master_ip,
+            'PORT': '1521',
+        },
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'qinggang',
+            'USER': 'root',
+            'PASSWORD': '123456',
+            'HOST': master_ip,
+            'PORT': '3306',
+        },
+        'mes_backup': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME': 'mesdbdg',#sid:mesdb2;service:mesdb
+            'USER': 'report_query',
+            'PASSWORD': 'xxx',
+            'HOST': '10.30.0.160',
+            'PORT': '1521',
+        },
+        'l2_backup': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME': 'qgil2dbdg',
+            'USER': 'report_query',
+            'PASSWORD': 'xxx',
+            'HOST': '10.30.0.161',
+            'PORT': '1521',
+        },
+        'sale': {
+            'ENGINE': 'django.db.backends.oracle',
+            'NAME':'mesdbdg',
+            'USER': 'report_query',
+            'PASSWORD': 'xxxx',
+            'HOST': '10.30.0.160',
+            'PORT': '1521',
+        },
+    }
+    PROJECT_DIR = '/home/hadoop/qinggang/managesys'
+    MEDIA_ROOT = '/home/hadoop/qinggang/media/'
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = '/home/hadoop/qinggang/static/'
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(PROJECT_DIR, 'static'),
