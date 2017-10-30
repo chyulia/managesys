ee8f4d8dc43514c72531f9ecc05e24a60458e1d1
changxincheng <chengcx1019@gmail.com>
2017-10-30 21:56:57 +0800

    页面遮罩效果

---
+++ data_import/static/data_import/js/noaction.js
@@ -0,0 +1,14 @@
+//$(function(){
+    function noAction(){
+//        $('.bg').css({'display':'block'});
+//        $('.content').css({'display':'block'});
+          $('.bg').fadeIn(200);
+		  $('.content').fadeIn(400);
+    }
+
+
+    function removeNoAction(){
+        $('.bg').fadeOut(200);
+		$('.content').fadeOut(200);
+    }
+//});
